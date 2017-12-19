a18c0b6400cc2a8f7968a6e022d45ca0653e9731
OSSportal <ossportal2017@gmail.com>
2017-12-19 11:22:53 +0900

    서브 배너 출력 방식 수정

--- components/SSCSnb/views/widget.blade.php
+++ components/SSCSnb/views/widget.blade.php
@@ -1,1 +1,4 @@
+{{--<p><a href="{{ url($item->link) }}" target="{{ $item->link_target }}"><img src="{{ $item->imageUrl() }}" width="225" height="58" alt="{{ $item->title }}"></a></p>--}}
+@foreach ($items as $item)
 <p><a href="{{ url($item->link) }}" target="{{ $item->link_target }}"><img src="{{ $item->imageUrl() }}" width="225" height="58" alt="{{ $item->title }}"></a></p>
+@endforeach
